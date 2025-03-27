82105f210ab30423ce3b2eb265659686cfcb954e
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2025-03-27 08:47:33 -0500

    Se estructuró el dashboard

---
+++ dashboard/view.php
@@ -0,0 +1,42 @@
+
+<link rel="stylesheet" href="../blocks/learning_style/dashboard/css/style.css">
+<div class="block_term">
+    <div class="flex">
+        <div class="icon_term">
+            <img src="../blocks/learning_style/dashboard/assets/ent.png" alt="">
+        </div>
+        <div class="value">10/30</div>
+    </div>
+    <b>
+        Encuestados
+    </b>
+</div>
+<div class="block_term">
+    <div class="flex">
+        <div class="icon_term">
+            <img src="../blocks/learning_style/dashboard/assets/grupo.png" alt="">
+        </div>
+        <div class="value">Activo</div>
+    </div>
+    <b>
+        Estilo dominante
+    </b>
+</div>
+<div class="block_term">
+    <div class="flex">
+        <div class="icon_term">
+            <img src="../blocks/learning_style/dashboard/assets/solo.png" alt="">
+        </div>
+        <div class="value">Reflexivo</div>
+    </div>
+    <b>
+        Estilo menos dominante
+    </b>
+</div>
+
+<canvas id="distr_pie" class="graf_term"></canvas>
+<canvas id="distr_bar" class="graf_term"></canvas>
+
+<script src="../blocks/learning_style/dashboard/js/chart.js"></script>
+<script src="../blocks/learning_style/dashboard/js/main.js"></script>
+
