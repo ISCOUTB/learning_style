17970ad761e8363e124fd26a7fa8da83618f8b51
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2025-03-27 16:37:42 -0500

    Se solucionó el error de height infinito, y se cargó el grafico de barras

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,7 +1,50 @@
-let ctx_bar_ = document.getElementById("distr_pie").getContext("2d");
-let ctx_bar_pie = document.getElementById("distr_bar").getContext("2d");
-
-barChartSales = new Chart(ctx_bar_, {
+document.addEventListener("DOMContentLoaded",()=>{
+  let ctx_bar_ = document.getElementById("distr_bar").getContext("2d");
+  let ctx_pie = document.getElementById("distr_pie").getContext("2d");
+  
+  let pieChart = new Chart(ctx_pie, {
+      type: "pie",
+      data: {
+        labels: ["Activo", "Reflexivo", "Verbal", "Global", "Secuencial","Intuitivo", "Sensitivo", "Visual"],
+        datasets: [
+          {
+            label: "Distribución de estilos de aprendizaje",
+            data: [30, 25,10,15,9,12,29,20],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: "Distribución de estilos de aprendizaje",
+          },
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+  let barChart = new Chart(ctx_bar_, {
     type: "bar",
     data: {
       labels: ["Activo", "Reflexivo", "Verbal", "Global", "Secuencial","Intuitivo", "Sensitivo", "Visual"],
@@ -32,15 +75,10 @@ barChartSales = new Chart(ctx_bar_, {
     options: {
       responsive: true,
       maintainAspectRatio: false,
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
       plugins: {
         title: {
           display: true,
-          text: "Estilos de aprendizaje",
+          text: "Distribución de estilos de aprendizaje",
         },
         legend: {
           display: false,
@@ -48,3 +86,4 @@ barChartSales = new Chart(ctx_bar_, {
       },
     },
   });
+})
