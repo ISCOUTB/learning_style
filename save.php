ed52836dd36b00e27e8ecd6a1178dc47a7619dd9
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2025-03-31 14:28:47 -0500

    Se guardan los valores numericos de cada par de estilo de aprendizaje de manera atomica dependiendo de la inclinación del estudiante, se adicionaron 8 variables para cada par que almacenan un n > 0 OR 0 (indica que no se inclina por el estilo)

--- save.php
+++ save.php
@@ -10,21 +10,29 @@ $act_ref_eval = [1,5,9,13,17,21,25,29,33,37,41];
 $act_ref["a"] = 0;
 $act_ref["b"] = 0;
 $act_ref["result"] = "";
+$act = 0;
+$ref = 0;
 
 $sen_int_eval = [2,6,10,14,18,22,26,30,34,38,42];
 $sen_int["a"] = 0;
 $sen_int["b"] = 0;
 $sen_int["result"] = "";
+$sen = 0;
+$int = 0;
 
 $vis_vrb_eval = [3,7,11,15,19,23,27,31,35,39,43];
 $vis_vrb["a"] = 0;
 $vis_vrb["b"] = 0;
 $vis_vrb["result"] = "";
+$vis = 0;
+$vrb = 0;
 
 $seq_glo_eval = [4,8,12,16,20,24,28,32,36,40,44];
 $seq_glo["a"] = 0;
 $seq_glo["b"] = 0;
 $seq_glo["result"] = "";
+$seq = 0;
+$glo = 0;
 
 for ($i=1;$i<=44;$i++){
     $learning_style_a[$i] = optional_param("learning_style:q".$i, 0, PARAM_INT);
@@ -50,8 +58,10 @@ foreach($act_ref_eval as $item){
 
 if ($act_ref["a"]>$act_ref["b"]) {
     $act_ref["result"] = ($act_ref["a"]-$act_ref["b"])."a";
+    $act = $act_ref["a"]-$act_ref["b"];
 }else{
     $act_ref["result"] = ($act_ref["b"]-$act_ref["a"])."b";
+    $ref = $act_ref["b"]-$act_ref["a"];
 }
 
 /**
@@ -68,8 +78,10 @@ foreach($sen_int_eval as $item){
 
 if ($sen_int["a"]>$sen_int["b"]) {
     $sen_int["result"] = ($sen_int["a"]-$sen_int["b"])."a";
+    $sen = $sen_int["a"]-$sen_int["b"];
 }else{
     $sen_int["result"] = ($sen_int["b"]-$sen_int["a"])."b";
+    $int = $sen_int["b"]-$sen_int["a"];
 }
 
 /**
@@ -86,8 +98,10 @@ foreach($vis_vrb_eval as $item){
 
 if ($vis_vrb["a"]>$vis_vrb["b"]) {
     $vis_vrb["result"] = ($vis_vrb["a"]-$vis_vrb["b"])."a";
+    $vis = $vis_vrb["a"]-$vis_vrb["b"];
 }else{
     $vis_vrb["result"] = ($vis_vrb["b"]-$vis_vrb["a"])."b";
+    $vrb = $vis_vrb["b"]-$vis_vrb["a"];
 }
 
 /**
@@ -104,8 +118,10 @@ foreach($seq_glo_eval as $item){
 
 if ($seq_glo["a"]>$seq_glo["b"]) {
     $seq_glo["result"] = ($seq_glo["a"]-$seq_glo["b"])."a";
+    $seq = $seq_glo["a"]-$seq_glo["b"];
 }else{
     $seq_glo["result"] = ($seq_glo["b"]-$seq_glo["a"])."b";
+    $glo = $seq_glo["b"]-$seq_glo["a"];
 }
 
 /*
@@ -123,7 +139,7 @@ echo "----- ".$seq_glo["result"]." -----";
 */
 $redirect = new moodle_url('/course/view.php', array('id'=>$courseid));
 
-if(save_learning_style($courseid,$act_ref["result"],$sen_int["result"],$vis_vrb["result"],$seq_glo["result"],$act_ref["a"],$act_ref["b"],$sen_int["a"],$sen_int["b"],$vis_vrb["a"],$vis_vrb["b"],$seq_glo["a"],$seq_glo["b"])){
+if(save_learning_style($courseid,$act_ref["result"],$sen_int["result"],$vis_vrb["result"],$seq_glo["result"],$act,$ref,$sen,$int,$vis,$vrb,$seq,$glo)){
     redirect($redirect, get_string('redirect_accept_success', 'block_learning_style') );
 }else{
     redirect($redirect, get_string('redirect_accept_exist', 'block_learning_style') );
