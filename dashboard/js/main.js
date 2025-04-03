8e7bcc26bcf49b8b9ec3cf375b9b9ff334e0e54f
Lisandro Zapata <85231001+LisoProgrammer@users.noreply.github.com>
2025-04-03 15:14:41 -0500

    Se cargan los estudiantes encuestados y el estilo de aprendizaje dominante

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,11 +1,55 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  let total_enc = document.getElementById("total_enc");
+  let est_dom = document.getElementById("est_dom");
   let request_get_metrics = await fetch("../blocks/learning_style/dashboard/api/get_metrics.php");
   if (request_get_metrics.ok) {
     let response_get_metrics = await request_get_metrics.json();
     console.log(response_get_metrics)
+    let total_curso = response_get_metrics["total_students_on_course"];
+    let enc = response_get_metrics["total_students"]
+    //total_enc.innerText = ((enc/total_curso)*100) + "%";//
+    total_enc.innerText = enc + "/" + total_curso;
     let ctx_bar_ = document.getElementById("distr_bar").getContext("2d");
     let ctx_pie = document.getElementById("distr_pie").getContext("2d");
+    let llave_max = "";
+    let estilo_hu = "";
+    let max_value = 0;
+    for(let estilo in response_get_metrics){
+      //console.log(estilo);
+      if(response_get_metrics[estilo] > max_value){
+        llave_max = estilo;
+        max_value = response_get_metrics[estilo];
+      }
+    }
+    switch(llave_max){
+      case "num_act":
+        estilo_hu = "Activo";
+        break;
+      case "num_ref":
+        estilo_hu = "Reflexivo";
+        break;
+      case "num_vis":
+        estilo_hu = "Visual";
+        break;
+      case "num_vrb":
+        estilo_hu = "Verbal";
+        break;
+      case "num_sen":
+        estilo_hu = "Sensorial";
+        break;
+      case "num_int":
+        estilo_hu = "Intuitivo";
+        break;
+      case "num_sec":
+        estilo_hu = "Secuencial";
+        break;
+      case "num_glo":
+        estilo_hu = "Global";
+        break; 
 
+    }
+    console.log("estilo dominante ", llave_max);
+    est_dom.innerText = estilo_hu;
     let pieChart = new Chart(ctx_pie, {
       type: "pie",
       data: {
@@ -92,4 +136,3 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   }
 })
-
