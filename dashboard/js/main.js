f56187ce4f32fea7fa6099db4c51303ddd1b0f6c
Kevin Guerrero <92352993+HKevinGuerrero@users.noreply.github.com>
2025-04-04 12:17:13 -0500

    Logica estilo dom y menos dom

--- dashboard/js/main.js
+++ dashboard/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
   let total_enc = document.getElementById("total_enc");
   let est_dom = document.getElementById("est_dom");
+  let est_men_dom = document.getElementById("est_men_dom");
   let request_get_metrics = await fetch("../blocks/learning_style/dashboard/api/get_metrics.php");
   if (request_get_metrics.ok) {
     let response_get_metrics = await request_get_metrics.json();
@@ -11,16 +12,28 @@ document.addEventListener("DOMContentLoaded", async () => {
     total_enc.innerText = enc + "/" + total_curso;
     let ctx_bar_ = document.getElementById("distr_bar").getContext("2d");
     let ctx_pie = document.getElementById("distr_pie").getContext("2d");
+    
     let llave_max = "";
+    let llave_min = "";
     let estilo_hu = "";
+    let estilo_men = "";
     let max_value = 0;
+    let min_value = 0;
+
+    //Calculo estilo dominante y menos dominante 
     for(let estilo in response_get_metrics){
       //console.log(estilo);
       if(response_get_metrics["data"][estilo] > max_value){
         llave_max = estilo;
         max_value = response_get_metrics[estilo];
       }
+      if (response_get_metrics["data"][estilo] < min_value) {
+        llave_min = estilo;
+        min_value = response_get_metrics["data"][estilo];
+      }
     }
+
+    //Determina estilo dominante
     switch(llave_max){
       case "num_act":
         estilo_hu = "Activo";
@@ -50,8 +63,45 @@ document.addEventListener("DOMContentLoaded", async () => {
         estilo_hu = "N/A";
         break;
     }
+
+    //Determina estilo menos dominante
+    switch (llave_min) {
+      case "num_act":
+        estilo_men = "Activo";
+        break;
+      case "num_ref":
+        estilo_men = "Reflexivo";
+        break;
+      case "num_vis":
+        estilo_men = "Visual";
+        break;
+      case "num_vrb":
+        estilo_men = "Verbal";
+        break;
+      case "num_sen":
+        estilo_men = "Sensorial";
+        break;
+      case "num_int":
+        estilo_men = "Intuitivo";
+        break;
+      case "num_sec":
+        estilo_men = "Secuencial";
+        break;
+      case "num_glo":
+        estilo_men = "Global";
+        break;
+      default:
+        estilo_men = "N/A";
+        break;
+    }
+
+    //Muestra resulstados
     console.log("estilo dominante ", llave_max);
     est_dom.innerText = estilo_hu;
+    console.log("Estilo menos dominante: ", llave_min);
+    est_men_dom.innerText = estilo_men;
+
+    //Grafico
     let labels = [];
     let data = [];
     data.push(response_get_metrics["data"]["num_act"]);
