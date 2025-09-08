e8227865f10dfe8ecd88717c5596bafe2dbff190
Server Dev <server@saviodev.utb.edu.co>
2025-09-08 23:29:12 +0000

    Fix ZIP package structure for Moodle installation

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2025090807;
+$plugin->version = 2025090808;
 $plugin->requires = 2013050100;
 $plugin->component = 'block_learning_style'; // Full name of the plugin (used for diagnostics)
 $plugin->release   = '1.2';
