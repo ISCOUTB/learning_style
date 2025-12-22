a72928a5728c90623bef961d62a5e9e3481b8f54
Santiago Orejuela <sorejuela18@gmail.com>
2025-12-22 00:22:32 -0500

    Add permissions for learning style block actions

--- db/access.php
+++ db/access.php
@@ -8,28 +8,36 @@ $capabilities = array(
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
-
         'clonepermissionsfrom' => 'moodle/my:manageblocks'
     ),
+
+    // Permiso para agregar el bloque en un curso
     'block/learning_style:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
-
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         ),
-
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
-    'block/learning_style:viewresults' => array(
+
+    'block/learning_style:viewreports' => array(
         'captype' => 'read',
         'contextlevel' => CONTEXT_COURSE,
         'archetypes' => array(
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
+        )
+    ),
+
+    'block/learning_style:take_test' => array(
+        'captype' => 'write', 
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'student' => CAP_ALLOW
         )
     ),
 );
